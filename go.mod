9d88a1e0e1e309907b194a054019089f613a25a0
Nathan Paul <mail@polarhive.net>
2024-02-25 00:10:42 +0530

    feat: parse markdown

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/polarhive/ssg
 
 go 1.22.0
+
+require github.com/yuin/goldmark v1.7.0 // indirect
--- main.go
+++ main.go
@@ -1,16 +1,25 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
 	"path/filepath"
+
+	"github.com/yuin/goldmark"
+	"github.com/yuin/goldmark/extension"
+	"github.com/yuin/goldmark/parser"
+	"github.com/yuin/goldmark/renderer/html"
 )
 
 // copy logic
 func copyDir(src, dst string) error {
+	if _, err := os.Stat(src); os.IsNotExist(err) {
+		return fmt.Errorf("source directory %s does not exist", src)
+	}
 	if err := os.MkdirAll(dst, 0755); err != nil {
 		return err
 	}
@@ -45,23 +54,60 @@ func copyDir(src, dst string) error {
 }
 
 func main() {
-	// serve flag
 	serveFlag := flag.Bool("serve", false, "only run ssg serve after copying files")
 	flag.Parse()
 
-	// copy stuff from static/* -> rendered/* (for now)
+	// copy files from static/* to rendered/* (for now)
 	srcDir := "static"
 	dstDir := "rendered"
+
+	// check for files in the static folder
 	if err := copyDir(srcDir, dstDir); err != nil {
 		fmt.Println("error copying directory:", err)
 		os.Exit(1)
 	}
-	fmt.Println("site built successfully!")
+
+	// parse markdown
+	md := goldmark.New(
+		goldmark.WithExtensions(extension.GFM),
+		goldmark.WithParserOptions(
+			parser.WithAutoHeadingID(),
+		),
+		goldmark.WithRendererOptions(
+			html.WithHardWraps(),
+		),
+	)
+
+	// read content
+	markdownContent, err := os.ReadFile("content/post.md")
+	if err != nil {
+		fmt.Println("error reading markdown file:", err)
+		os.Exit(1)
+	}
+
+	// parse markdown content
+	var htmlContent bytes.Buffer
+	if err := md.Convert(markdownContent, &htmlContent); err != nil {
+		fmt.Println("error converting markdown to HTML:", err)
+		os.Exit(1)
+	}
+
+	// write HTML to disk
+	outputFile, err := os.Create("rendered/index.html")
+	if err != nil {
+		fmt.Println("error creating output file:", err)
+		os.Exit(1)
+	}
+	defer outputFile.Close()
+	if _, err := io.Copy(outputFile, &htmlContent); err != nil {
+		fmt.Println("error writing HTML content to output file:", err)
+		os.Exit(1)
+	}
 
 	// serve the rendered/* dir
 	if *serveFlag {
 		http.Handle("/", http.FileServer(http.Dir("rendered")))
-		fmt.Println("server started at http://localhost:8080")
+		fmt.Println("Server started at http://localhost:8080")
 		if err := http.ListenAndServe(":8080", nil); err != nil {
 			fmt.Println("Error starting server:", err)
 			os.Exit(1)
