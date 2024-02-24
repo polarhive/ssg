db33b8a023f6d24f01eaa982d13ffb95a968382a
Nathan Paul <mail@polarhive.net>
2024-02-24 23:17:52 +0530

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/polarhive/ssg
+
+go 1.22.0
---
+++ main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+)
+
+func copyDir(src, dst string) error {
+	entries, err := os.ReadDir(src)
+	if err != nil {
+		return err
+	}
+
+	for _, entry := range entries {
+		srcPath := filepath.Join(src, entry.Name())
+		dstPath := filepath.Join(dst, entry.Name())
+
+		if entry.IsDir() {
+			if err := copyDir(srcPath, dstPath); err != nil {
+				return err 
+			}
+		} else {
+			if err := copyFile(srcPath, dstPath); err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+func copyFile(src, dst string) error {
+	srcFile, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer srcFile.Close()
+
+	dstFile, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer dstFile.Close()
+
+	_, err = io.Copy(dstFile, srcFile)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func main() {
+	srcDir := "static"
+	dstDir := "rendered"
+
+	if err := copyDir(srcDir, dstDir); err != nil {
+		fmt.Println("Error copying directory:", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Directory copied successfully!")
+}
