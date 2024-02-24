34a16b9ead99b37a510bd0ff1fab1dcd5572a721
Nathan Paul <mail@polarhive.net>
2024-02-24 23:45:04 +0530

    feat: serve flag

--- main.go
+++ main.go
@@ -1,65 +1,70 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io"
+	"net/http"
 	"os"
 	"path/filepath"
 )
 
+// copy logic
 func copyDir(src, dst string) error {
+	if err := os.MkdirAll(dst, 0755); err != nil {
+		return err
+	}
 	entries, err := os.ReadDir(src)
 	if err != nil {
 		return err
 	}
-
 	for _, entry := range entries {
 		srcPath := filepath.Join(src, entry.Name())
 		dstPath := filepath.Join(dst, entry.Name())
-
 		if entry.IsDir() {
 			if err := copyDir(srcPath, dstPath); err != nil {
-				return err 
+				return err
 			}
 		} else {
-			if err := copyFile(srcPath, dstPath); err != nil {
+			srcFile, err := os.Open(srcPath)
+			if err != nil {
+				return err
+			}
+			defer srcFile.Close()
+			dstFile, err := os.Create(dstPath)
+			if err != nil {
+				return err
+			}
+			defer dstFile.Close()
+			if _, err := io.Copy(dstFile, srcFile); err != nil {
 				return err
 			}
 		}
 	}
-
-	return nil
-}
-
-func copyFile(src, dst string) error {
-	srcFile, err := os.Open(src)
-	if err != nil {
-		return err
-	}
-	defer srcFile.Close()
-
-	dstFile, err := os.Create(dst)
-	if err != nil {
-		return err
-	}
-	defer dstFile.Close()
-
-	_, err = io.Copy(dstFile, srcFile)
-	if err != nil {
-		return err
-	}
-
 	return nil
 }
 
 func main() {
+	// serve flag
+	serveFlag := flag.Bool("serve", false, "only run ssg serve after copying files")
+	flag.Parse()
+
+	// copy stuff from static/* -> rendered/* (for now)
 	srcDir := "static"
 	dstDir := "rendered"
-
 	if err := copyDir(srcDir, dstDir); err != nil {
-		fmt.Println("Error copying directory:", err)
+		fmt.Println("error copying directory:", err)
 		os.Exit(1)
 	}
+	fmt.Println("site built successfully!")
 
-	fmt.Println("Directory copied successfully!")
+	// serve the rendered/* dir
+	if *serveFlag {
+		http.Handle("/", http.FileServer(http.Dir("rendered")))
+		fmt.Println("server started at http://localhost:8080")
+		if err := http.ListenAndServe(":8080", nil); err != nil {
+			fmt.Println("Error starting server:", err)
+			os.Exit(1)
+		}
+	}
 }
